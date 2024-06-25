03bee331459d94e400f6a012957d1c23df97a416
Pharadol <pharadol.n@gmail.com>
2024-06-26 02:44:52 +0700

    chore: add setup tailwind.css

---
+++ src/components/์Navbar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>Hello</h1>
+  <div>
+    <p>dasfasf</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  @apply text-red-300;
+  p {
+    @apply p-2;
+  }
+}
+</style>
