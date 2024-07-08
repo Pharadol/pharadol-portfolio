ce9e6545bfa0612c8800ef04e079e712f16c8120
Pharadol <pharadol.n@gmail.com>
2024-07-08 17:44:21 +0700

    feat: test access img path

---
+++ src/assets/data/projectsLists.js
@@ -0,0 +1,25 @@
+const pathImg = "/src/assets/images/projects";
+
+
+export const projectList = [
+  {
+    title: "Sawang",
+    link: "https://www.youtube.com",
+    thumbnail: `${pathImg}/Sawang.png`,
+  },
+  {
+    title: "Image Slider",
+    link: "https://www.youtube.com",
+    thumbnail: `${pathImg}/ImageSlider.png`,
+  },
+  {
+    title: "Expense Tracker React",
+    link: "https://www.youtube.com",
+    thumbnail: `${pathImg}/ExpenseTrackerReact.png`,
+  },
+  {
+    title: "Card Matching Game",
+    link: "https://www.youtube.com",
+    thumbnail: `${pathImg}/CardMatchingGame.png`,
+  },
+];
